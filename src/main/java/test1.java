9048afc1fb08f2d6d84412179bdc4b58b2c0537f
ViKi <viktorija.kutsenko@gmail.com>
2023-11-28 00:13:41 +0200

    initial commit4

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -1,9 +1,13 @@
 import com.github.javafaker.Faker;
 
+import java.util.Locale;
+
 public class test1 {
     public static void main(String[] args) {
-       System.out.println("hello wordlik");
-        System.out.println(Faker.instance().animal().name());
-        System.out.println(Faker.instance().dog().name());
+       System.out.println("Hello in worldik beautiful "
+               + Faker.instance().animal().name().toUpperCase()
+               + " with name "
+               + Faker.instance().dog().name().toUpperCase());
+
     }
 }
