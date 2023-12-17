3502fc4fe6f5809539eed753b91afab0d852e5f0
ViKi <viktorija.kutsenko@gmail.com>
2023-12-17 23:41:40 +0200

    commit7

--- src/main/java/Lesson4/Triangle.java
+++ src/main/java/Lesson4/Triangle.java
@@ -1,4 +1,24 @@
 package Lesson4;
 
+import java.util.Scanner;
+
 public class Triangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the quantity of rows for triangle: ");
+        int rows = scanner.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            int m = 1;
+            while (m <= 2 * i - 1) {
+                System.out.print("*");
+                m ++;
+            }
+            System.out.println();
+        }
+    }
 }
